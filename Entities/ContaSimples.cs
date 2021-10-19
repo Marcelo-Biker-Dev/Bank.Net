65f6d18e12b503cc49d1f8b89d5bbcfd2e06a615
Marcelo Santos <marcelo.biker.dev@gmail.com>
2021-10-19 02:09:12 +0100

    Implementado classe ContaSimples

--- Entities/ContaSimples.cs
+++ Entities/ContaSimples.cs
@@ -1,7 +1,29 @@
+using System;
+
 namespace Bank.Net.Entities
 {
-    public class ContaSimples
+    public class ContaSimples : Conta
     {
         
+		private static int geradorDeNumeracaoDeContas = 1001; // Usado para gerar novos de números de Contas Simples e Contas Empresa
+		private static string prefixoContaSimples = "S"; // Usado para gerar novos de números de Contas Simples e Contas Empresa
+
+		public ContaSimples(decimal depositoInicial, string nome)
+		{
+			this.DepositoInicial = depositoInicial;
+			this.Nome = nome;
+			this.Id = GeradorDeIdDeContas();
+			System.Console.WriteLine("dentro do construtor");
+ 			Depositar(this.DepositoInicial, DateTime.Now, inicioDeConta);
+			System.Console.WriteLine($"Conta #{this.Id} iniciada com sucesso!");
+		}
+
+		protected override string GeradorDeIdDeContas()
+		{
+			string id = prefixoContaSimples + geradorDeNumeracaoDeContas.ToString();
+			geradorDeNumeracaoDeContas++;
+
+			return id;
+		}
     }
 }
