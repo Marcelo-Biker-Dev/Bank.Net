05cf58696e499898b5a9b7ed19764fa4b3689e08
Marcelo Santos <marcelo.biker.dev@gmail.com>
2021-10-19 02:04:23 +0100

    Implementado classe Credito

--- Entities/Credito.cs
+++ Entities/Credito.cs
@@ -1,7 +1,37 @@
+using System;
+
 namespace Bank.Net.Entities
 {
     public class Credito
     {
-        
+        private decimal Valor { get; }
+        private DateTime Data { get; }
+        private string Observacao { get; }
+
+        public Credito(decimal valor, DateTime data, string observacao)
+        {
+            this.Valor = valor;
+            this.Data = data;
+            this.Observacao = observacao;
+        }
+
+        public static decimal credito(string idConta)
+        {
+            decimal creditoInicial = 0;
+
+            if (idConta.StartsWith("S"))
+            {
+                creditoInicial = (decimal)NiveisDeCredito.CreditoPadraoContaSimples;
+            }
+
+            if (idConta.StartsWith("E"))
+            {
+                creditoInicial = (decimal)NiveisDeCredito.CreditoPadraoContaEmpresa;
+            }
+
+            return creditoInicial;
+        }
+        public decimal getValor()
+        {return this.Valor; }
     }
 }
